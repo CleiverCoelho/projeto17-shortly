064ce95a238a55680dc536767c46498892914d30
Cleiver <creibis10@gmail.com>
2023-05-20 17:01:11 -0300

    feat: login usuario geração de token de sessao e senhas criptografas

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,14 +1,14 @@
 import { Router } from "express"
 
 import validateSchema from "../middlewares/validateSchema.middleware.js"
-import { signUpSchema } from "../schemas/signUp.schemas.js"
-import { signUpUser } from "../controllers/users.cotrollers.js"
-import { validateSignUpEmail } from "../middlewares/users.middleware.js"
+import { signUpSchema, signInSchema } from "../schemas/users.schemas.js"
+import { signUpUser, signInUser } from "../controllers/users.cotrollers.js"
+import { validateSignUpEmail, validateSignIn } from "../middlewares/users.middleware.js"
 
 const usersRouter = Router()
 
 usersRouter.post("/sign-up", validateSchema(signUpSchema), validateSignUpEmail, signUpUser)
-// customersRouter.get("/customers/:id", validateGetCustomer, getCustomerById)
+usersRouter.post("/sign-in", validateSchema(signInSchema), validateSignIn,signInUser)
 // customersRouter.post("/customers", validateSchema(customerSchema), validateCustomerCpf, createCustomer)
 // customersRouter.put("/customers/:id", validateSchema(customerSchema),validateCustomerCpf,  updateCustomer)
 
--- src/schemas/users.schemas.js
+++ src/schemas/users.schemas.js
@@ -6,3 +6,10 @@ export const signUpSchema = joi.object({
     password: joi.string().required(),
     confirmPassword: joi.string().valid(joi.ref('password')).required()
 })
+
+export const signInSchema = joi.object({
+    email: joi.string().email().trim().required(),
+    password: joi.string().required(),
+})
+
+
