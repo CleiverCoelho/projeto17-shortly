05d87c8f0a5d052bdfbc4c4eb149a327d5d00173
Cleiver <creibis10@gmail.com>
2023-05-20 16:22:32 -0300

    feat: verificacao unicidade de email feita pela api em um middleware

--- src/middlewares/users.middleware.js
+++ src/middlewares/users.middleware.js
@@ -1,25 +1,11 @@
 import { db } from "../database/database.connection.js"
 
-export async function validateGetCustomer(req, res, next) {
-    const { id } = req.params
+export async function validateSignUpEmail(req, res, next) {
+    const { email } = req.body
     try {
-        const customer = await db.query(`SELECT * FROM customers WHERE id=$1`, [id])
-        if (customer.rowCount === 0) return res.status(404).send({ message: "Esse usuário não existe!" })
-        res.locals.customer = customer.rows[0]
-        next()
-    } catch (err) {
-        res.status(500).send(err.message)
-    }
-}
-
-export async function validateCustomerCpf(req, res, next) {
-    const { cpf } = req.body
-    const { id } = req.params
-    try {
-        const customer = await db.query(`SELECT * FROM customers WHERE cpf=$1`, [cpf])
-        if (customer.rowCount === 0) return next()
-        if (customer.rowCount > 0 && customer.rows[0].id === Number(id)) return next()
-        return res.status(409).send({ message: "Esse usuário já existe!" })
+        const user = await db.query(`SELECT * FROM users WHERE email=$1`, [email])
+        if (user.rowCount === 0) return next()
+        return res.status(409).send({ message: "Email informado já existe!" })
     } catch (err) {
         res.status(500).send(err.message)
     }
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -3,10 +3,11 @@ import { Router } from "express"
 import validateSchema from "../middlewares/validateSchema.middleware.js"
 import { signUpSchema } from "../schemas/signUp.schemas.js"
 import { signUpUser } from "../controllers/users.cotrollers.js"
+import { validateSignUpEmail } from "../middlewares/users.middleware.js"
 
 const usersRouter = Router()
 
-usersRouter.post("/sign-up", validateSchema(signUpSchema), signUpUser)
+usersRouter.post("/sign-up", validateSchema(signUpSchema), validateSignUpEmail, signUpUser)
 // customersRouter.get("/customers/:id", validateGetCustomer, getCustomerById)
 // customersRouter.post("/customers", validateSchema(customerSchema), validateCustomerCpf, createCustomer)
 // customersRouter.put("/customers/:id", validateSchema(customerSchema),validateCustomerCpf,  updateCustomer)
