31ddbde5b0e2a7fbe77857d62a36faed4ce2ef06
Cleiver <creibis10@gmail.com>
2023-05-21 08:24:50 -0300

    connected to database deploy

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,11 +4,14 @@ import validateSchema from "../middlewares/validateSchema.middleware.js"
 import { signUpSchema, signInSchema } from "../schemas/users.schemas.js"
 import { signUpUser, signInUser } from "../controllers/users.cotrollers.js"
 import { validateSignUpEmail, validateSignIn } from "../middlewares/users.middleware.js"
+import { getAllUsers } from "../controllers/urls.controllers.js"
 
 const usersRouter = Router()
 
 usersRouter.post("/sign-up", validateSchema(signUpSchema), validateSignUpEmail, signUpUser)
 usersRouter.post("/sign-in", validateSchema(signInSchema), validateSignIn,signInUser)
+usersRouter.get("/users", getAllUsers)
+
 // customersRouter.post("/customers", validateSchema(customerSchema), validateCustomerCpf, createCustomer)
 // customersRouter.put("/customers/:id", validateSchema(customerSchema),validateCustomerCpf,  updateCustomer)
 
