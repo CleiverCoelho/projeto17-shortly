cd0281ad80eeefe0cf44ae51cdc356e1e7198a13
Cleiver <creibis10@gmail.com>
2023-05-22 15:24:55 -0300

    fix: organizando ranking em uma unica query

--- src/controllers/ranking.controllers.js
+++ src/controllers/ranking.controllers.js
@@ -3,20 +3,18 @@ import { db } from "../database/database.connection.js"
 export async function getRanking(req, res) {
     try {
         const {rows: ranking} = await db.query(`SELECT users.id, users.name, 
-        SUM(urls.views) AS "visitCount" 
+        SUM(urls.views) AS "visitCount", COUNT(urls.id) AS "linksCount"
         FROM urls
         JOIN users ON users.id=urls."userId"
-        GROUP BY users.id, users.name`)
+        GROUP BY users.id, users.name
+        ORDER BY "visitCount" DESC LIMIT 10`)
 
-        const {rows: linksCount} = await db.query(`SELECT COUNT(urls.id) AS "linksCount" 
-        FROM urls
-        JOIN users ON users.id=urls."userId"
-        GROUP BY users.name`)
         
-        const response = ranking.map((res, index) => {
-            return {...res, ...linksCount[index]}
-        })
-        res.status(201).send(response);
+        
+        // const response = ranking.map((res, index) => {
+        //     return {...res, ...linksCount[index]}
+        // })
+        res.status(201).send(ranking);
     } catch (err) {
         res.status(500).send(err.message)
     }
