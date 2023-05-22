6049a25f68b98abeac7f3432c4417c3b384f47c9
Cleiver <creibis10@gmail.com>
2023-05-22 11:05:21 -0300

    changed2 dump_sql

--- dump.sql
+++ dump.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 12.14
+-- Dumped from database version 12.14 (Ubuntu 12.14-0ubuntu0.20.04.1)
 -- Dumped by pg_dump version 12.14 (Ubuntu 12.14-0ubuntu0.20.04.1)
 
 SET statement_timeout = 0;
@@ -16,20 +16,6 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
---
--- Name: public; Type: SCHEMA; Schema: -; Owner: -
---
-
-CREATE SCHEMA public;
-
-
---
--- Name: SCHEMA public; Type: COMMENT; Schema: -; Owner: -
---
-
-COMMENT ON SCHEMA public IS 'standard public schema';
-
-
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
@@ -40,7 +26,7 @@ SET default_table_access_method = heap;
 
 CREATE TABLE public.sessions (
     id integer NOT NULL,
-    "userId" integer NOT NULL,
+    "userId" integer,
     token text NOT NULL,
     "createdAt" timestamp without time zone DEFAULT now() NOT NULL
 );
@@ -108,8 +94,7 @@ CREATE TABLE public.users (
     id integer NOT NULL,
     name text NOT NULL,
     email text NOT NULL,
-    password text NOT NULL,
-    "createdAt" timestamp without time zone DEFAULT now() NOT NULL
+    password text NOT NULL
 );
 
 
@@ -158,48 +143,46 @@ ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_
 -- Data for Name: sessions; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-INSERT INTO public.sessions VALUES (1, 1, '7d3e5c0d-2ee5-41b0-9c2f-56c90394b428', '2023-05-21 11:21:37.860757');
-INSERT INTO public.sessions VALUES (2, 2, '8f81ca03-50e3-4d41-b0f1-764648674c88', '2023-05-21 11:22:53.149931');
+INSERT INTO public.sessions VALUES (10, 4, '8cad354f-ff04-41d9-8cb2-273fa8e61778', '2023-05-20 17:05:37.842073');
+INSERT INTO public.sessions VALUES (11, 4, '33651a06-52fb-4cc7-9167-9b57dbe1514b', '2023-05-20 17:46:30.350404');
+INSERT INTO public.sessions VALUES (12, 4, 'eab4a0ae-cf80-4751-847d-5a199f196764', '2023-05-20 19:07:48.059232');
+INSERT INTO public.sessions VALUES (13, 5, '2e840391-eb61-4952-b91e-c68791269a4b', '2023-05-20 20:37:47.628256');
 
 
 --
 -- Data for Name: urls; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-INSERT INTO public.urls VALUES (2, 'https://dashboard.render.com/d/dpg-chkvkjm7avj2178982b0-a/info', 1, '0eaaoy', '2023-05-21 11:22:30.474075', 4);
-INSERT INTO public.urls VALUES (4, 'https://www.notion.so/bootcampra/Tutorial-Deploy-de-aplica-es-back-end-no-Render-PostgreSQL-524e395a023a4da096c032c9b12dac67', 2, 'kDOP9C', '2023-05-21 11:26:02.952739', 12);
-INSERT INTO public.urls VALUES (1, 'https://access.redhat.com/documentation/pt-br/red_hat_enterprise_linux/8/html/deploying_different_types_of_servers/backing-up-postgresql-data_using-postgresql', 1, '8PCDYU', '2023-05-21 11:22:17.578531', 2);
-INSERT INTO public.urls VALUES (5, 'https://www.amazon.com.br/?tag=admarketbr-20&ref=pd_sl_587192828264c05fdc17879ec7488cfa49dadd80fff91ef795f66c0f&mfadid=adm', 2, 'ejf185', '2023-05-21 12:56:08.764804', 0);
-INSERT INTO public.urls VALUES (6, 'https://www.google.com/search?channel=fs&client=ubuntu&q=google+tradutor', 2, 'NDEcQT', '2023-05-21 12:56:27.888931', 4);
+INSERT INTO public.urls VALUES (1, 'https://www.figma.com/file/DWg9233KR2GS6RLvfZRwyd/Shortly?type=design&node-id=0-1', 4, '7KBz5l', '2023-05-20 19:50:39.842826', 1);
 
 
 --
 -- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: -
 --
 
-INSERT INTO public.users VALUES (1, 'teste', 'teste@teste.com', '$2b$10$/ygLwyy.nP2rGjtATjYM.eXOXdKQ.7v6g0Of8zOEBYgErPMJ4zXDm', '2023-05-21 11:21:33.490127');
-INSERT INTO public.users VALUES (2, 'cleiver', 'cleiver@cleiver.com', '$2b$10$AaF6ZcrSkNlv3.Zkt3L.sOkGvQxywaYP5GHbReyylHpzoNscyRj2O', '2023-05-21 11:22:45.167751');
+INSERT INTO public.users VALUES (4, 'cleiver1', 'cleiver@cleiver.com', '$2b$10$chGE6NXp9EkDwmu6gapgmuOkMVPVsOPNhMO05jHzpR09cLahBohe.');
+INSERT INTO public.users VALUES (5, 'teste', 'teste@teste.com', '$2b$10$/5Z72DQrc0Uiu6RUfaLyGuBvp/Ygmeu/dol6KuNuMqtvYBjbWUKzq');
 
 
 --
 -- Name: sessions_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.sessions_id_seq', 2, true);
+SELECT pg_catalog.setval('public.sessions_id_seq', 13, true);
 
 
 --
 -- Name: urls_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.urls_id_seq', 6, true);
+SELECT pg_catalog.setval('public.urls_id_seq', 3, true);
 
 
 --
 -- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.users_id_seq', 2, true);
+SELECT pg_catalog.setval('public.users_id_seq', 5, true);
 
 
 --
@@ -216,6 +199,14 @@ ALTER TABLE ONLY public.sessions
 
 ALTER TABLE ONLY public.urls
     ADD CONSTRAINT urls_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: users users_email_key; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_email_key UNIQUE (email);
 
 
 --
@@ -240,16 +231,6 @@ ALTER TABLE ONLY public.sessions
 
 ALTER TABLE ONLY public.urls
     ADD CONSTRAINT "urls_userId_fkey" FOREIGN KEY ("userId") REFERENCES public.users(id);
-
-
---
--- Name: SCHEMA public; Type: ACL; Schema: -; Owner: -
---
-
-REVOKE ALL ON SCHEMA public FROM postgres;
-REVOKE ALL ON SCHEMA public FROM PUBLIC;
-GRANT ALL ON SCHEMA public TO cleiver;
-GRANT ALL ON SCHEMA public TO PUBLIC;
 
 
 --
